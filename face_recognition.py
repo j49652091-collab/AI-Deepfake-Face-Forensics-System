f6c4320a958f8dff434d1c33d128bc99b4c3dcf4
j49652091-collab <j49652091@gmail.com>
2026-05-23 11:59:44 +0300

    Create face_recognition.py

---
+++ face_recognition.py
@@ -0,0 +1,32 @@
+from deepface import DeepFace
+
+
+def compare_faces(img1,img2):
+
+    try:
+
+        result=DeepFace.verify(
+            img1_path=img1,
+            img2_path=img2,
+            model_name="Facenet",
+            enforce_detection=False
+        )
+
+        similarity=round(
+            (1-result["distance"])*100,
+            2
+        )
+
+        status="Match Found" if result["verified"] else "No Match"
+
+        return {
+            "similarity":max(0,similarity),
+            "status":status
+        }
+
+    except:
+
+        return {
+        "similarity":0,
+        "status":"Face Detection Failed"
+        }
